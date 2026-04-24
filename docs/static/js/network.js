04146693e38837364e4a256f904541afa8d9cabc
DiseaseModelling <franka.hofgarten@web.de>
2026-04-24 12:13:49 +0200

    Update network.js

--- docs/static/js/network.js
+++ docs/static/js/network.js
@@ -11,7 +11,7 @@ window.addEventListener("resize", resize);
 // ================= CONFIG =================
 const NUM_NODES = 160;
 const MAX_DIST = 190;
-const GROWTH_SPEED = 0.01;
+const GROWTH_SPEED = 0.005;
 
 // ================= NODES =================
 const nodes = [];
